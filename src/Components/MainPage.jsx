77782c7905cd8f54b5a0471cdf603fa9d9a21399
Artemis <gayatri@artemisinfratech.com>
2025-09-17 18:20:19 +0530

    updated

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -53,10 +53,10 @@ const FlatInventory = () => {
 const fetchFlats = async () => {
   try {
     // --localhost
-    const res = await fetch("http://localhost:5000/api/flats");   
+    // const res = await fetch("http://localhost:5000/api/flats");   
     
 // netlify :
-    // const res = await fetch("/api/flats");                              
+    const res = await fetch("/api/flats");                              
 
     let data = await res.json();
     const now = new Date();
@@ -141,9 +141,9 @@ const [formData, setFormData] = useState({
     } else {
       payload.holdUntil = null;
     }
-    // const res = await fetch(`/api/flats/${selectedFlat.flatKey}`, {
+    const res = await fetch(`/api/flats/${selectedFlat.flatKey}`, {
     // localhost:
-    const res = await fetch(`http://localhost:5000/api/flats/${selectedFlat.flatKey}`, {
+    // const res = await fetch(`http://localhost:5000/api/flats/${selectedFlat.flatKey}`, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(payload),
