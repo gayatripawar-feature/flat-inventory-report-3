981e7ed2d16739296f1bedefe31f7a1b970b13eb
Artemis <gayatri@artemisinfratech.com>
2025-09-19 16:08:16 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -212,7 +212,7 @@ app.put("/api/flats/:flatKey", async (req, res) => {
 // Serve frontend
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("/*/", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
