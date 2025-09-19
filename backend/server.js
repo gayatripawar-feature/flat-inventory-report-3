6d6616d182642a4bef01b79d8a7fdd2ad1cfd0f9
Artemis <gayatri@artemisinfratech.com>
2025-09-19 18:02:52 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -215,4 +215,5 @@ app.use(express.static(path.join(__dirname, "../frontend/dist")));
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
+
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
