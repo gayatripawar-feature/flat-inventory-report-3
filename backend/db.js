f2262f2968dda48aeee8a1af555a3d097d61565b
Artemis <gayatri@artemisinfratech.com>
2025-09-19 14:22:36 +0530

    updated

---
+++ backend/db.js
@@ -0,0 +1,24 @@
+// const mysql = require("mysql2/promise");
+// require("dotenv").config();
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+// });
+
+// module.exports = pool;
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+export default pool;
